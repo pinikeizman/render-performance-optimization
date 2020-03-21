dab7d2af64b7688a87773159ab17287dbcc7447c
pinikeizman <pini@bigpanda.io>
2020-03-21 22:13:52 +0200

    Reduce memory usage
    print buffer transfering to console.

--- public/worker.js
+++ public/worker.js
@@ -1,14 +1,18 @@
 /** Apply filter, multiply the RED property for each pixel by X. */
 function applyFilter(buffer) {
     const startTime = Date.now();
-    const filteredBuffer = buffer.map((value, index) => index % 4 === 0 ? value * 1.25 : value);
+     buffer.forEach((value, index) => {
+        if(index % 4 === 0)
+            buffer[index] =  value * 1.25
+    });
     console.log(`Worker: Handler took: ${Date.now() - startTime}ms`);
-    return filteredBuffer;
+    return buffer;
 }
 
 const processImage = ({width, height, data}) => new ImageData(applyFilter(data), width, height);
 
 addEventListener('message', ({data}) => {
     const imageData = processImage(data);
-    postMessage(imageData, [imageData.data.buffer])
+    postMessage(imageData, [imageData.data.buffer]);
+    console.log('first', imageData.data.buffer, 'sec', data.data.buffer)
 });
